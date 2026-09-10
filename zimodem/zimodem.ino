c9cdb8cf450cb6297f915a4ce955f7c642da0aa1
rcdev67 <andreas@falmer.de>
2026-09-10 23:25:57 +0200

    SuperMini: 11 dBm, the middle ground between a failing join and a lossy link

--- zimodem/zimodem.ino
+++ zimodem/zimodem.ino
@@ -415,7 +415,7 @@ static bool connectWifi(const char* ssid, const char* password, IPAddress *ip, I
   debugPrintf("WiFi join to '%s' (pw %d chars)...\r\n", ssid, (int)strlen(password));
   WiFi.begin(ssid, password);
 #if defined(ARDUINO_MAKERGO_C3_SUPERMINI) || defined(ARDUINO_NOLOGO_ESP32C3_SUPER_MINI)
-  WiFi.setTxPower(WIFI_POWER_8_5dBm);  // the SuperMini's antenna is badly matched, more power makes it worse
+  WiFi.setTxPower(WIFI_POWER_11dBm);   // the SuperMini's antenna is badly matched: 15dBm broke the join, 8.5dBm lost 12% of packets
   WiFi.setSleep(false);                // modem sleep drops the link every few minutes on the C3
 #endif
   if(hostname.length() > 0)
